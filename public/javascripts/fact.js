4a5627ba459fd1b56868643e62945bb63f242e1b
dustindo1 <duhstin0@gmail.com>
2025-11-05 15:30:34 -0800

    update

--- public/javascripts/fact.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("generate").addEventListener("click", async () => {
-  const response = await fetch("https://catfact.ninja/fact");
-  const data = await response.json();
-  document.getElementById("fact-box").textContent = data.fact;
-});
--- random-generator/public/javascripts/facts.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("generate").addEventListener("click", async () => {
-  const response = await fetch("https://catfact.ninja/fact");
-  const data = await response.json();
-  document.getElementById("fact-box").textContent = data.fact;
-});
-console.log('facts.js loaded successfully!');

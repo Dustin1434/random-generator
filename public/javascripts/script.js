e133884089cea250d44a449c8f622067f0a3209e
dustindo1 <duhstin0@gmail.com>
2025-11-05 17:29:00 -0800

    update

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,15 @@
-const generateButton = document.getElementById('generate_button');
+const items = [
+  "Believe in yourself!",
+  "You can do it!",
+  "Never give up!",
+  "Keep learning!"
+];
 
-generateButton.addEventListener('click', function() {
-    window.location.reload();
+
+const generateButton = document.getElementById('generate_button');
+const display = document.getElementById('random-item');
+
+generateButton.addEventListener('click', function () {
+    const randomItem = items[Math.floor(Math.random() * items.length)];
+    display.textContent = randomItem;
 });
